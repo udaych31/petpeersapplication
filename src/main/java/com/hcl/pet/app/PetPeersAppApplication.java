6759a7e7ec467dd714817c9931a75ac32333121c
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-29 14:25:17 +0530

    initial commit for project setup

---
+++ src/main/java/com/hcl/pet/app/PetPeersAppApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.pet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPeersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPeersAppApplication.class, args);
+	}
+
+}
